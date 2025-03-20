b919204bcb99703bbef87c7a5cd93ced4faf92ca
Sergio Lavanga <slavanga@users.noreply.github.com>
2025-03-20 23:22:50 +0100

    Implement new services section

--- src/js/components/services.js
+++ src/js/components/services.js
@@ -5,21 +5,16 @@ gsap.registerPlugin(ScrollTrigger);
 
 (() => {
   gsap.fromTo(
-    document.querySelectorAll('.services__item'),
+    document.querySelectorAll('.services__title'),
     {
-      '--line-progress': 0,
       '--bg-progress': '100%',
     },
     {
-      '--line-progress': 1,
       '--bg-progress': '0%',
-      stagger: {
-        each: 0.05,
-      },
       scrollTrigger: {
         trigger: document.querySelector('.section--services'),
-        start: 'top top',
-        end: 'bottom center+=20%',
+        start: 'top center',
+        end: 'center center-=10%',
         scrub: 1,
       },
     },
