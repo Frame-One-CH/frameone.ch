c7967f5c0bd84ab99f9c6b856c0efa2f187bd4c4
Sergio Lavanga <slavanga@users.noreply.github.com>
2026-09-17 17:32:02 +0200

    Modernize color syntax and SCSS conditionals

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   extends: ['stylelint-config-standard-scss'],
   rules: {
-    'alpha-value-notation': null,
     'at-rule-empty-line-before': null,
-    'color-function-notation': null,
     'selector-class-pattern': null,
-    'scss/at-rule-conditional-no-parentheses': null,
-    'scss/double-slash-comment-empty-line-before': null,
   },
 };
